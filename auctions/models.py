c22fb5e5ba90daff3d341d41938cf7059c9958ab
gutieYpong <ponglai22@gmail.com>
2020-11-25 20:11:02 +0800

    1st COMMIT. Still at start progress.

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    item_image = models.ImageField(upload_to='images/', blank=True, null=True)
+    item_name = models.CharField(max_length=64)
+    item_desc = models.CharField(max_length=128)
+    starting_bid = models.FloatField()
+
+    def __str__(self):
+        return f"Item {self.id}: {self.item_name}"
+
+# def get_image_path(instance, filename):
+#     return os.path.join('photos', str(instance.id), filename)
--- commerce/settings.py
+++ commerce/settings.py
@@ -119,4 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     BASE_DIR + "/static/",
+# ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
